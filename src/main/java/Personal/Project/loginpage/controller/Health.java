9a64c7e80cf2440afa0f2a06fad662dfeee58112
Sergio <sergioajk@hotmail.com>
2023-11-29 17:09:13 -0300

    feat: check

--- src/main/java/Personal/Project/loginpage/controller/Health.java
+++ src/main/java/Personal/Project/loginpage/controller/Health.java
@@ -1,6 +1,15 @@
 package Personal.Project.loginpage.controller;
 
-//teste
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/health")
 public class Health {
+  @GetMapping("/check")
+  public String sayHello() {
+    return "Hello World";
+  }
+
 }
