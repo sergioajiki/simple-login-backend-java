759863bf26de0316a0f3e252390315b2ba948b34
Sergio <sergioajk@hotmail.com>
2023-11-29 20:24:28 -0300

    feat: UserRepository

---
+++ src/main/java/personal/project/loginpage/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package personal.project.loginpage.repository;
+
+import personal.project.loginpage.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
