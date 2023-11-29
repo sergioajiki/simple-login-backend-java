745c3e850fa458bf165d9c37d2847b462bb146a0
Sergio <sergioajk@hotmail.com>
2023-11-29 20:58:30 -0300

    feat<UserService>: create User

--- src/main/java/personal/project/loginpage/service/UserService.java
+++ src/main/java/personal/project/loginpage/service/UserService.java
@@ -1,5 +1,6 @@
 package personal.project.loginpage.service;
 
+import personal.project.loginpage.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import personal.project.loginpage.repository.UserRepository;
@@ -11,5 +12,9 @@ public class UserService {
   @Autowired
   public UserService(UserRepository userRepository) {
     this.userRepository = userRepository;
+  }
+
+  public User create(User user) {
+    return userRepository.save(user);
   }
 }
