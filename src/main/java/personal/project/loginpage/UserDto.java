52551bea93889b68eaa7733d025f7d879759248c
Sergio <sergioajk@hotmail.com>
2023-11-29 21:40:44 -0300

    feat<UserDto>: userDto to user, user to UserDto

--- src/main/java/personal/project/loginpage/UserDto.java
+++ src/main/java/personal/project/loginpage/UserDto.java
@@ -1,9 +1,30 @@
 package personal.project.loginpage;
 
+import personal.project.loginpage.entity.User;
 public record UserDto(
   Long id,
   String username,
   String email,
   String password,
   String role
-) {}
+) {
+  public static UserDto userToUserDto(User user) {
+    return new UserDto(
+        user.getId(),
+        user.getUsername(),
+        user.getEmail(),
+        user.getPassword(),
+        user.getRole()
+    );
+  }
+
+  public static User userDtoToUser(UserDto userDto) {
+    User user = new User();
+    user.setId(userDto.id());
+    user.setUsername(userDto.username());
+    user.setEmail(userDto.email());
+    user.setPassword(userDto.password());
+    user.setRole(userDto.role());
+    return user;
+  }
+}
