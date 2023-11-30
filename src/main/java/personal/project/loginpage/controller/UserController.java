87479719f07df78ec60dd7dcd7be1073b548863d
Sergio <sergioajk@hotmail.com>
2023-11-30 11:35:47 -0300

    feat<UserController>: Post /users

--- src/main/java/personal/project/loginpage/controller/UserController.java
+++ src/main/java/personal/project/loginpage/controller/UserController.java
@@ -1,8 +1,12 @@
 package personal.project.loginpage.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import personal.project.loginpage.entity.User;
 import personal.project.loginpage.service.UserService;
 
 @RestController
@@ -12,5 +16,11 @@ public class UserController {
   @Autowired
   public UserController(UserService userService) {
     this.userService = userService;
+  }
+
+  @PostMapping()
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+    User savedUser = userService.create(user);
+    return ResponseEntity.status(201).body(savedUser);
   }
 }
