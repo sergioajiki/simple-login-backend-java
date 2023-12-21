b75a07975fd63f79ba2e79a9d2a276b75d06df2d
Sergio <sergioajk@hotmail.com>
2023-12-21 17:55:52 -0300

    feat<TokenService>: generateExpirationDate

--- src/main/java/personal/project/loginpage/service/TokenService.java
+++ src/main/java/personal/project/loginpage/service/TokenService.java
@@ -3,12 +3,17 @@ package personal.project.loginpage.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TokenService {
+
   @Value("${api.security.token.secret}")
   private String codeSecret;
 
@@ -16,6 +21,14 @@ public class TokenService {
     Algorithm algorithm = Algorithm.HMAC256(codeSecret);
     return JWT.create()
         .withSubject(userDetails.getUsername())
+        .withExpiresAt(generateExpirationDate())
         .sign(algorithm);
-         }
+  }
+
+  private Instant generateExpirationDate() {
+    return LocalDateTime.now()
+        .plusHours(2)
+        .toInstant(ZoneOffset.of("-03:00"));
+  }
+
 }
