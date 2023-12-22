284f567a16f1c4933b9daaab61f5c843d934af5f
Sergio <sergioajk@hotmail.com>
2023-12-22 17:53:40 -0300

    feat<SecurityConfig>: selecionando rotas para autorização

--- src/main/java/personal/project/loginpage/security/SecurityConfig.java
+++ src/main/java/personal/project/loginpage/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package personal.project.loginpage.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,10 +13,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import personal.project.loginpage.repository.SecurityFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+  private final SecurityFilter securityFilter;
+
+  @Autowired
+  public SecurityConfig(SecurityFilter securityFilter) {
+    this.securityFilter = securityFilter;
+  }
 
   //  configuração básica
   @Bean
@@ -26,6 +37,16 @@ public class SecurityConfig {
             //    não guarda o state
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         )
+        .authorizeHttpRequests(
+            authorize -> authorize
+                .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
+                .anyRequest().authenticated()
+        )
+        .addFilterBefore(
+            securityFilter,
+            UsernamePasswordAuthenticationFilter.class
+        )
         .build();
   }
 
@@ -33,6 +54,7 @@ public class SecurityConfig {
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
+
   @Bean
   public AuthenticationManager authenticationManager(
       AuthenticationConfiguration authenticationConfiguration
