972d01dfa4662c301e2fcad3436c8ee3cc2cb447
Sergio <sergioajk@hotmail.com>
2023-12-27 16:30:15 -0300

    build: configurando dependencia para utilizar o swagger

--- src/main/java/personal/project/loginpage/security/SecurityConfig.java
+++ src/main/java/personal/project/loginpage/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package personal.project.loginpage.security;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
